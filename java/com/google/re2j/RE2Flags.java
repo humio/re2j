bd855d98931ec61723d66789cc78f2ffa9f6eacb
Erik Søe Sørensen <erik@humio.com>
2020-12-12 01:43:38 +0100

    Add RE2Flags, for controlling debug logging verbosity.

---
+++ java/com/google/re2j/RE2Flags.java
@@ -0,0 +1,7 @@
+package com.google.re2j;
+
+public class RE2Flags {
+  public static boolean verboseCompiler = false;
+  public static boolean verboseOptimizer = false;
+  public static boolean verboseRuntime = false;
+}
